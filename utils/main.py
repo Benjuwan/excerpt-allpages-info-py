f2c12367d9d1168179003c3fcdf7f77f65086d06
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2025-07-09 09:11:06 +0900

    fix: 検出対象を同一オリジンのみならず他のドメインも含める処理分岐を追加及びREADMEの更新.

--- utils/main.py
+++ utils/main.py
@@ -2,7 +2,7 @@ from website_scraping import website_scraping
 from filtered_keyword import filtered_keyword
 from generate_xlsx import generate_xlsx
 
-# スクレイピングしたいwebサイトのURL（と任意でスクレイピング処理対象ページ数）を指定
+# 第一引数：`対象サイトのURL`, 第二引数（任意）：`検出対象ページ数`（デフォルト = 100件）, 第三引数（任意）：`検出対象は同一オリジン限定か他のドメインも含めるか`（デフォルト = `True` -> 同一オリジン限定）
 website_scraping_result = website_scraping("https://example-company.co.jp", 5)
 if website_scraping_result is not None:
     print(f"総計 {len(website_scraping_result)} ページをスクレイピング")
